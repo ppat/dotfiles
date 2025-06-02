e3dd7af106ad2ae447569d31c5e4d820c22c40b6
Peter Pathirana <peter@latent.io>
2025-06-02 18:39:21 +0000

    fix: fix up macos install

--- .chezmoitemplates/mise_install.sh
+++ .chezmoitemplates/mise_install.sh
@@ -1,11 +1,14 @@
 # shellcheck shell=bash
 
 mise_install() {
-  log_info "Language SDKs & Tools | Activating Mise package manager..."
   if ! command -v mise > /dev/null; then
     log_error "Language SDKs & Tools | Mise binary is not in path, cannot proceed!"
     exit 1
   fi
+  log_info "Language SDKs & Tools | Using mise env..."
+  env | grep MISE | sort | sed -E 's|^(.*)|    \1|g'
+  echo
+  log_info "Language SDKs & Tools | Activating Mise package manager..."
   eval "$(mise activate bash)" 2>&1 | sed -E 's|^(.*)|    \1|g'
   log_info "Language SDKs & Tools | Installing or upgrading mise packages..."
   mise upgrade --yes 2>&1 | sed -E 's|^(.*)|    \1|g'
