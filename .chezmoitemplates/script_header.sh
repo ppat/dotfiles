6de18cd5575dd608d99780a834eaf5829b0b9914
Peter Pathirana <peter@latent.io>
2025-06-22 22:39:55 +0000

    fix: ensure that all environment variable references in chezmoi scripts are taken from templated .env files

--- .chezmoitemplates/script_header.sh
+++ .chezmoitemplates/script_header.sh
@@ -1,5 +1,4 @@
 # shellcheck shell=bash
-
 log_info() {
   echo -e "\033[0;36m[INFO]\033[0m $1"      # Bright cyan
 }
@@ -15,12 +14,3 @@ log_warning() {
 log_error() {
   echo -e "\033[1;91m[ERROR]\033[0m $1"     # Bright red with bold
 }
-
-set -o allexport
-if [[ -e $HOME/.env ]]; then
-  . $HOME/.env
-fi
-if [[ -e $HOME/.env.overrides ]]; then
-  . $HOME/.env.overrides
-fi
-set +o allexport
