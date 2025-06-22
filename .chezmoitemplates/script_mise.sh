eecc26d96a1fabd6a45d60e0b162f6543741033d
Peter Pathirana <peter@latent.io>
2025-06-22 18:56:34 -0400

    fix: fix mise + aqua package installation issues due to first-time vs ongoing updates

---
+++ .chezmoitemplates/script_mise.sh
@@ -0,0 +1,31 @@
+# shellcheck shell=bash
+mise_prerequisites() {
+  local setup_type="$1"
+  if ! command -v mise > /dev/null; then
+    log_error "Mise | $setup_type | Mise binary is not in path, cannot proceed!"
+    exit 1
+  fi
+}
+
+show_mise_env() {
+  local setup_type="$1"
+  log_info "Mise | $setup_type | Using mise env..."
+  env | grep '^MISE' | sort | sed -E 's|^(.*)|    \1|g'
+}
+
+display_installed_mise_packages() {
+  local setup_type="$1"
+  log_success "Mise | $setup_type | Display installed packages..."
+  mise ls 2>&1 | sed -E 's|^(.*)|    \1|g'
+}
+
+setup_mise_packages() {
+  local setup_type="$1"
+
+  log_info "Mise | $setup_type | Activating Mise..."
+  eval "$(mise activate bash)" 2>&1 | sed -E 's|^(.*)|    \1|g'
+
+  log_info "Mise | $setup_type | Installing or upgrading mise packages..."
+  mise upgrade --yes 2>&1 | sed -E 's|^(.*)|    \1|g'
+  log_success "Mise | $setup_type | Mise install is complete."
+}
