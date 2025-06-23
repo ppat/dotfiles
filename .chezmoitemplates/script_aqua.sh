a7521a3b5511ec782211010161db53bd066fa70d
Peter Pathirana <peter@latent.io>
2025-06-23 00:00:36 +0000

    fix: fix first-time vs standard setup for mise + aqua

---
+++ .chezmoitemplates/script_aqua.sh
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+aqua_prerequisites() {
+  local setup_type="$1"
+  log_info "Aqua | $setup_type | Initializing Aqua..."
+  if ! command -v aqua > /dev/null; then
+    log_error "Aqua | $setup_type | Aqua binary is not in path, cannot proceed!"
+    exit 1
+  fi
+}
+
+show_aqua_env() {
+  local setup_type="$1"
+  log_info "Aqua | $setup_type | Using aqua env..."
+  env | grep '^AQUA' | sort | sed -E 's|^(.*)|    \1|g'
+}
+
+setup_aqua_packages() {
+  local setup_type="$1"
+  log_info "Aqua | $setup_type | Installing or upgrading aqua packages..."
+  aqua install --all 2>&1 | sed -E 's|^(.*)|  \1|g'
+  log_success "Aqua | $setup_type | Aqua install is complete."
+}
+
+display_installed_aqua_packages() {
+  local setup_type="$1"
+  log_info "Aqua | $setup_type | Display installed packages..."
+  aqua list --installed --all | column -t 2>&1 | sed -E 's|^(.*)|  \1|g'
+}
