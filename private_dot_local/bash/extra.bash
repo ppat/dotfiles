719c12e981b8ebcda102ff3f67a2245f727cbce9
Peter Pathirana <peter@latent.io>
2025-09-08 19:41:07 +0000

    perf: improve shell performance

--- private_dot_local/bash/extra.bash
+++
@@ -1,6 +0,0 @@
-# Additional Bash customizations
-
-if [[ -f $HOME/.krew/bin/kubectl-cnpg ]]; then
-  ln -sf $HOME/.krew/bin/kubectl-cnpg $HOME/.local/bin/cnpg
-  source <($HOME/.local/bin/cnpg completion bash)
-fi
--- private_dot_local/bash/shell-completions.bash
+++ private_dot_local/bash/shell-completions.bash
@@ -13,72 +13,29 @@ if ! shopt -oq posix; then
   fi
 fi
 
-# kubectl
-if command -v kubectl > /dev/null; then
-  source <(kubectl completion bash)
-  alias k=kubectl
-  complete -o default -F __start_kubectl k
+command_specific_setup() {
+  case $1 in
+    kubectl)
+      alias k=kubectl
+      complete -o default -F __start_kubectl k
+      ;;
+    *)
+      ;;
+  esac  
+}
+
+if [[ -d "$HOME/.local/bash-completions" ]]; then
+  while IFS= read -r -d '' _completion_file; do
+    current_command="${_completion_file##*/}"
+    if command -v ${current_command} > /dev/null; then
+      source "${_completion_file}"
+      command_specific_setup "${current_command}"
+    fi
+  done < <(find "$HOME/.local/bash-completions" -type f -print0)
+  unset _completion_file
 fi
-
-# helm
-if command -v helm > /dev/null; then
-  source <(helm completion bash)
-fi
-
-# flux
-if command -v flux > /dev/null; then
-  source <(flux completion bash)
-fi
-
-# gh
-if command -v gh > /dev/null; then
-  source <(gh completion -s bash)
-fi
-
-# kustomize
-# if command -v kustomize > /dev/null; then
-#   source <(kustomize completion bash)
-# fi
-
-# terraform
-# if [[ -d "$TFENV_CONFIG_DIR/versions/" ]]; then
-#   if find $TFENV_CONFIG_DIR/versions/ -name terraform -type f -executable > /dev/null; then
-#     # shellcheck disable=SC2012
-#     complete -C $TFENV_CONFIG_DIR/versions/$(ls $TFENV_CONFIG_DIR/versions | sort -rn | head -1)/terraform terraform
-#   fi
-# fi
-
-# yq
-# if command -v yq > /dev/null; then
-#   source <(yq shell-completion bash)
-# fi
-
-# # poetry
-# if command -v poetry > /dev/null; then
-#   source <(poetry completions bash)
-# fi
-
-# # pip
-# if command -v pip > /dev/null; then
-#   source <(pip completion --bash)
-# fi
-
-# kind
-# if command -v kind > /dev/null; then
-#   source <(kind completion bash)
-# fi
 
 # zoxide
 # if command -v zoxide > /dev/null; then
-#   source <(zoxide init bash)
-# fi
-
-# Mise completions
-# if command -v mise &> /dev/null; then
-#  eval "$(mise completion bash)"
-# fi
-
-# Aqua completions
-# if command -v aqua &> /dev/null; then
-#  source <(aqua completion bash)
+#   source <()
 # fi
